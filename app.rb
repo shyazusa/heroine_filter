cc035dc2fb942cca107723d21d7e80f6502fa7d7
shy_azusa <shy.azusa@gmail.com>
2018-03-10 13:36:45 +0900

    photo upload done

--- app.rb
+++ app.rb
@@ -1,6 +1,33 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'slim'
 
 get '/' do
-  'hello world.'
+  @title = 'heroine_filter'
+  slim :index
+end
+
+post '/upload' do
+  @title = 'upload'
+  if params[:photo]
+    save_path = "./public/images/#{params[:photo][:filename]}"
+    File.open(save_path, 'wb') do |f|
+      p params[:photo][:tempfile]
+      f.write params[:photo][:tempfile].read
+      @mes = 'アップロード成功'
+    end
+  else
+    @mes = 'アップロード失敗'
+  end
+  slim :upload
+  redirect 'images'
+end
+
+get '/images' do
+  images_name = Dir.glob("./public/images/*")
+  @images_path = []
+  images_name.each do |image|
+    @images_path << image.gsub("public/", "./")
+  end
+  slim :images
 end
